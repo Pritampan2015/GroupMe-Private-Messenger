e6eee1f1c7449b1dc91e26aecefe086a535d37c7
Pritampan2015 <73078236+Pritampan2015@users.noreply.github.com>
2023-04-17 02:47:07 +0530

    update

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -21,7 +21,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://groupme-private-messenger.onrender.com";
 
 var socket, selectedChatCompare;
 
