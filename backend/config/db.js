ebb5ccf3567957d6415ee05a45475a686740a5b0
Pritampan2015 <73078236+Pritampan2015@users.noreply.github.com>
2023-04-21 11:53:53 +0530

    secure db

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected : ${conn.connection.host}`);
+    console.log(`MongoDB Connected : $conn.connection.host`);
   } catch (error) {
     console.log(`Error : ${error.message}`);
     process.exit;
